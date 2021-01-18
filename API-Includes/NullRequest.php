8dc1d90cb5ff8f557f6bb09f1447cd0d174f3900
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-01-18 16:08:17 +0300

    Create NullRequest.php

---
+++ API-Includes/NullRequest.php
@@ -0,0 +1,19 @@
+<?php
+/***********************************************************************************/
+/*                          (C) 2021 - Skyfallen                                   */
+/*                          Skyfallen SkyMake API                                  */
+/*                          The Skyfallen Company                                  */
+/*                                                                                 */
+/* This file is called when the request made does not resolve to a valid function  */
+/***********************************************************************************/
+
+// Check if our ABSPATH is defined
+defined("API_ABSPATH") or die("Don't mess!");
+
+// Get the global response file
+global $response;
+
+// Add the error message
+$response["status"]["code"] = 404;
+$response["status"]["message"] = "Not Found.";
+$response["error"]["description"] = "The requested API Endpoint does not exist.";
